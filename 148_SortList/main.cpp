e00114ea02f212488141acf7e1ec9f2f1fa39b5f
liulanglang22 <2659702668@qq.com>
2021-03-24 23:36:23 +0800

    day06

---
+++ 148_SortList/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+
+    }
+};
+int main() {
+    
+}
